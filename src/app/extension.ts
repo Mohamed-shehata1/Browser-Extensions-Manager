6e7b6022c26182b6b1b2147e38959fa3fe66183e
mohamed shehata <ms2217872@gmail.com>
2025-09-15 13:49:39 +0300

    changed Mocking the data from json server to  Angular for deployment on vecel

--- src/app/extension.ts
+++ src/app/extension.ts
@@ -1,15 +1,17 @@
 import { IExtension } from './models/extension';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 
 export class ExtensionService {
-  private url = "http://localhost:3000/extensions";
+  private url = "/db.json";
   constructor(private http: HttpClient) {}
 
   getAllExtensions(): Observable<IExtension[]> {
-    return this.http.get<IExtension[]>(this.url);
+    return this.http.get<{ extensions: IExtension[] }>(this.url).pipe(
+      map(data => data.extensions)
+    );
   }
 }
